3f3926452496f7e45c01318a57711fc6cd54fb50
Pavel Vorushilo <nexus5best5@gmail.com>
2019-10-02 16:55:08 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -38,8 +38,42 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    
+    let decodedWord = "";
+    let binWord = "";
+
+    for(let i = 0; i < expr.length; i += 10){
+        binWord = expr.substr(i,10);
+
+        if(binWord == "**********"){
+            decodedWord += " "; 
+        } else{
+        decodedWord += decodeSymbol(binWord);
+        }   
+        
+    }
+    return decodedWord;
 }
+
+
+function decodeSymbol(codeLet){
+    let decodedLet;
+    let binSymbol;
+    let morseLet = "";
+
+    for(let i = 0; i < 10; i += 2){
+        binSymbol = codeLet.substr(i, 2);
+        if(binSymbol == "10"){
+            morseLet += ".";
+        }else if(binSymbol == "11"){
+            morseLet += "-";
+        }
+        decodedLet = MORSE_TABLE[morseLet];
+    }
+
+    return decodedLet;
+}
+
 
 module.exports = {
     decode
